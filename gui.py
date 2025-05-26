378e46f0da02633d80971b489616cf457d5c0a01
Sage <kr4ckl3s@gmail.com>
2025-05-26 01:28:33 -0400

    Server connection fixed

--- gui.py
+++ gui.py
@@ -461,7 +461,7 @@ class UsernameCompiler:
             if getattr(sys, 'frozen', False):
                 base_path = sys._MEIPASS
                 server_path = os.path.join(base_path, 'server.exe')
-                proc = subprocess.Popen([server_path, str(port)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                proc = subprocess.Popen([server_path, str(port)])
             else:
                 base_path = os.path.dirname(os.path.abspath(__file__))
                 server_path = os.path.join(base_path, 'server.js')
